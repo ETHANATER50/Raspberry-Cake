f9fd74900789d43ca30df063890675e64dd9b4a2
ETHANATER50 <ETHANATER50@gmail.com>
2022-08-05 09:20:34 -0600

    started a login page with a lead on sessions

---
+++ RaspberryCake/RaspberryCake/Login.php
@@ -0,0 +1,29 @@
+<?php
+require "MyHeader.php";
+?>
+
+<br />
+<br />
+<form action="action_page.php" method="post">
+
+    <div class="container">
+        <label for="uname">
+            <b>Username</b>
+        </label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
+
+        <label for="psw">
+            <b>Password</b>
+        </label>
+        <input type="password" placeholder="Enter Password" name="psw" required />
+
+        <button type="submit">Login</button>
+    </div>
+
+
+
+</form>
+
+<?php
+require "MyFooter.php";
+?>
