b8cc75b176bad149d24f19b52536edff5bc90165
Xi-Xi <358xixi@gmail.com>
2022-08-08 09:32:25 -0600

    Delete page doneish
    how darest thou

--- RaspberryCake/RaspberryCake/Pages/DeletePage.php
+++ RaspberryCake/RaspberryCake/Pages/DeletePage.php
@@ -1,17 +1,23 @@
 <?php
 include_once "MyHeader.php";
 
+$url = $_SERVER['REQUEST_URI'];
+$url_components = parse_url($url);
+parse_str($url_components['query'], $params);
+
+echo $params['id'];
 ?>
 
 <!DOCTYPE html>
 <html>
 <body>
+    <h1>Are you sure you want to delete this product?</h1>
     <form action="../Delete.php" method="post">
-        <label id="idLabel">Product ID: </label><br />
         <p>
             <input type="submit" value="Delete" />
         </p>
     </form>
+    <button onclick="window.location.href='/jsonMulti.php'">Back</button>
 </body>
 </html>
 
