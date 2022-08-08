7c1e3a8f95f6a7c25839c1d2067b4c1ecf811358
ETHANATER50 <ethanater50@gmail.com>
2022-08-07 21:10:01 -0600

    login works now and makes a session

--- RaspberryCake/RaspberryCake/Login.php
+++ RaspberryCake/RaspberryCake/Login.php
@@ -4,25 +4,47 @@ require "MyHeader.php";
 
 <br />
 <br />
-<form action="action_page.php" method="post">
+<h2>Enter Username and Password</h2>
+<div class="container form-signin">
 
-    <div class="container">
-        <label for="uname">
-            <b>Username</b>
-        </label>
-        <input type="text" placeholder="Enter Username" name="uname" required />
+    <?php
+    $msg = '';
 
-        <label for="psw">
-            <b>Password</b>
-        </label>
-        <input type="password" placeholder="Enter Password" name="psw" required />
+    if (isset($_POST['login']) && !empty($_POST['username'])
+       && !empty($_POST['password'])) {
 
-        <button type="submit">Login</button>
-    </div>
+        if ($_POST['username'] == 'admin' &&
+           $_POST['password'] == 'admin') {
+            session_start();
+            $_SESSION['valid'] = true;
+            $_SESSION['timeout'] = time();
 
+            echo 'You have entered valid use name and password';
+        }else {
+            $msg = 'Wrong username or password';
+        }
+    }
+    ?>
+</div><!-- /container -->
 
+<div class="container">
 
-</form>
+    <form class="form-signin" role="form"
+        action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <h4 class="form-signin-heading">
+            <?php echo $msg; ?>
+        </h4>
+        <input type="text" class="form-control"
+            name="username" placeholder="username = admin"
+            required autofocus /><br />
+        <input type="password" class="form-control"
+            name="password" placeholder="password = admin" required />
+        <button class="btn btn-lg btn-primary btn-block" type="submit"
+            name="login">
+            Login
+        </button>
+    </form>
+</div>
 
 <?php
 require "MyFooter.php";
