86c06faf32a0943d905b04bb2c0a683c56a3deed
NicholasConforth <60490260+NicholasConforth@users.noreply.github.com>
2022-08-08 10:33:14 -0600

    Added other styles

--- RaspberryCake/RaspberryCake/index.php
+++ RaspberryCake/RaspberryCake/index.php
@@ -6,16 +6,38 @@ require "MyHeader.php";
 <br />
 <!DOCTYPE html>
 <html>
+
+<?php
+
+$css = "Styles/DarkMode.css";
+
+function ClickDark() {
+    echo $css = "Styles/LightMode.css";
+}
+function ClickLight() {
+    echo $css = "Styles/DarkMode.css";
+}
+function ClickBright() {
+    echo $css = "Styles/HighlighterMode.css";
+}
+?>
     <head>
-        <link rel="stylesheet" href="Styles/HighlighterMode.css" />
+        <link rel="stylesheet" href=<?php echo $css ?> />
     </head>
     <body>
-    
         <div>
-            <h1>This is sample text untill I get creative and figure out what I want to actutally write but untill then here is Cave Johnson''s lemon rant.</h1>
-            <p>All right, I've been thinking, when life gives you lemons, don't make lemonade! Make life take the lemons back! Get mad! I don't want your damn lemons! What am I supposed to do with these? Demand to see life's manager!
-                Make life rue the day it thought it could give Cave Johnson lemons! Do you know who I am? I'm the man whose gonna burn your house down - with the lemons!</p>
+            <input type="button" value="Dark" onclick="ClickDark()" />
+            <input type="button" value="Light" onclick="ClickLight()" />
+            <input type="button" value="Pain" onclick="ClickBright()" />
         </div>
+        <div id="body">
+            <h1>This is sample text untill I get creative and figure out what I want to actutally write but untill then here is Cave Johnson''s lemon rant.</h1>
+            <img src="Images/1.png" alt="2" id="image"/>
+            <p>
+                All right, I've been thinking, when life gives you lemons, don't make lemonade! Make life take the lemons back! Get mad! I don't want your damn lemons! What am I supposed to do with these? Demand to see life's manager!
+                Make life rue the day it thought it could give Cave Johnson lemons! Do you know who I am? I'm the man whose gonna burn your house down - with the lemons!
+            </p>
+    </div>
     </body>
 </html>
 
