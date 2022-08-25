e4055ea5c810f967e42cc110e55b41d94e94ba50
Xi-Xi <358xixi@gmail.com>
2022-08-25 09:48:41 -0600

    wow

--- RaspberryCake/RaspberryCake - Copy/jsonSingle.php
+++ RaspberryCake/RaspberryCake - Copy/jsonSingle.php
@@ -30,12 +30,49 @@ include_once "MyHeader.php";
         request.send();
     }
 
+    function shuffle(array) {
+        let currentIndex = array.length, randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
+    var correctAnswer;
+    const answers = [];
+
+    function checkAnswer(string) {
+        if (string === correctAnswer) {
+            /*add points*/
+            console.log('you did it');
+        }
+        else {
+            console.log('you FOOL');
+        }
+    }
+}
+
         // Run when the data has been loaded
     function loadComplete(evt) {
         var myResponse;
         var myData;
+        
+
+
+
         // create a table for display
-        var myReturn = "<table align='center'><tr><td><h1>Name &nbsp;  &nbsp; </h1></td><td><h1>Price &nbsp;  &nbsp; </h1></td></tr>";
+        //var myReturn = "<table align='center'><tr><td><h1>Name &nbsp;  &nbsp; </h1></td><td><h1>Price &nbsp;  &nbsp; </h1></td></tr>";
+        var myReturn = "<table align='center'><tr><td><h1>Question</h1></td></tr><tr><td><h2>What is...<h2></td></tr>"
 
         myResponse = request.responseText;
         //alert("A: " + myResponse); // Use for debug
@@ -43,19 +80,24 @@ include_once "MyHeader.php";
         myData = JSON.parse(myResponse);
 
         // Loop through each json record and create the HTML
-        for (index in myData) {
-            myReturn += "<tr><td><h1>" + myData[index].Name + "</h1></td><td><h1> $" +
-                myData[index].Price + "</h1></td></tr>";
-
+        for (let i = 4; i < 8; i++) {
+            //myReturn += "<tr><td><h1>" + myData[index].Name + "</h1></td><td><h1> $" +
+            //    myData[index].Price + "</h1></td></tr>";
+            if (i = 4) {
+                correctAnswer = myData[i];
+            }
+            answers.push(myData[i]);
         }
-        myReturn += "</table>";
+
+        shuffle(answers);
+
+        myReturn += "<tr><td><button onclick='checkAnswer(this.value)'>" + answers[0] + "</button></td><td><button onclick='checkAnswer(this.value)'>" + answers[1] + "</button></td></tr><tr><td><button onclick='checkAnswer(this.value)'>" + answers[2] + "</button></td><td><button onclick='checkAnswer(this.value)'>" + answers[3] + "</button></td></tr></table > ";
         document.getElementById("jsonData").innerHTML = myReturn; // Display table
     }
 
 
 </script>
 </div>
-<h1>"Want to purchase this product? Check out our contact page and place an order with us!"</h1>
 
 <?php
 include_once "MyFooter.php";
